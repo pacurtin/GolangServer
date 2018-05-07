464e27576e7b5947267043eb508b66624c4d8776
pacurtin <pacurtin@tcd.ie>
2018-05-07 19:56:32 +1000

    included mysql

--- server.go
+++ server.go
@@ -5,10 +5,15 @@ import (
 	"github.com/pacurtin/GolangServer/settings"
 	"github.com/codegangsta/negroni"
 	"net/http"
+	"database/sql"
 )
 
 func main() {
 	settings.Init()
+
+	// TODO move this to its own service(?)
+	db, err := sql.Open("mysql", "user:password@/dbname")
+
 	router := routers.InitRoutes()
 	n := negroni.Classic()
 	n.UseHandler(router)
